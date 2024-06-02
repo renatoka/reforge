2a9d1325a0c62bbeb553c2a3c43c425b405d772f
renatoka <rkauri1@vvg.hr>
2024-06-02 16:17:42 +0200

    chore: packages update, electron-forge refactor

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,13 +1,36 @@
-import { defineConfig } from 'vite';
+import type { ConfigEnv, UserConfig } from 'vite';
+import { defineConfig, mergeConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-import { pluginHotRestart } from './vite.base.config';
+import { external, getBuildConfig, getBuildDefine, pluginHotRestart } from './vite.base.config';
 
 // https://vitejs.dev/config
-export default defineConfig({
-  resolve: {
-    mainFields: ['module', 'jsnext:main', 'jsnext'],
-    browserField: false,
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
-  },
-  plugins: [pluginHotRestart('restart'), viteTsconfigPaths()],
+export default defineConfig(env => {
+  const forgeEnv = env as ConfigEnv<'build'>;
+  const { forgeConfigSelf } = forgeEnv;
+  const define = getBuildDefine(forgeEnv);
+  const config: UserConfig = {
+    build: {
+      lib: {
+        entry: forgeConfigSelf.entry!,
+        fileName: () => '[name].js',
+        formats: ['cjs'],
+      },
+      rollupOptions: {
+        external,
+      },
+    },
+    plugins: [pluginHotRestart('restart'), viteTsconfigPaths()],
+    define,
+    resolve: {
+      // Load the Node.js entry.
+      browserField: false,
+      mainFields: ['module', 'jsnext:main', 'jsnext'],
+      alias: {
+        '.prisma/client/default': './node_modules/@prisma/client/default.js',
+        '.prisma/client': './node_modules/@prisma/client/index.js',
+      },
+    },
+  };
+
+  return mergeConfig(getBuildConfig(forgeEnv), config);
 });
